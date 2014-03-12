607f7faf038168c512ec165c510714440017d649
Baylor Rae <baylorrae@gmail.com>
2014-03-12 08:35:32 -0500

    update requests to use new api

--- features/step_definitions/product_steps.rb
+++ features/step_definitions/product_steps.rb
@@ -1,134 +1,119 @@
 When(/^I add a new product with invalid brand$/) do
   VCR.use_cassette('product_invalid_brand') do
-    @product_response = ShipCompliant.client.call(:add_update_product, message: {
-      'Request' => {
-        'Security' => ShipCompliant.configuration.credentials,
-        'Product' => {
-          'BottleSizeMS' => 750,
-          'BrandKey' => 'DENSNW',
-          'DefaultCase' => 12,
-          'DefaultWholesaleCasePrice' => 270,
-          'Description' => 'Denver Snow Flake Cab 2013',
-          'PercentAlcohol' => 14.1,
-          'ProductDistribution' => 'Both',
-          'ProductKey' => 'SNFL13',
-          'ProductType' => 'Wine',
-          'UnitPrice' => 49.99,
-          'Varietal' => 'Cabernet Sauvignon',
-          'Vintage' => 2013,
-          'VolumeAmount' => 750,
-          'VolumeUnit' => 'milliliter'
-        },
-        'UpdateMode' => 'ErrorOnExisting'
-      }
+    @product_response = ShipCompliant.client.call(:add_update_product, {
+      'Product' => {
+        'BottleSizeMS' => 750,
+        'BrandKey' => 'DENSNW',
+        'DefaultCase' => 12,
+        'DefaultWholesaleCasePrice' => 270,
+        'Description' => 'Denver Snow Flake Cab 2013',
+        'PercentAlcohol' => 14.1,
+        'ProductDistribution' => 'Both',
+        'ProductKey' => 'SNFL13',
+        'ProductType' => 'Wine',
+        'UnitPrice' => 49.99,
+        'Varietal' => 'Cabernet Sauvignon',
+        'Vintage' => 2013,
+        'VolumeAmount' => 750,
+        'VolumeUnit' => 'milliliter'
+      },
+      'UpdateMode' => 'ErrorOnExisting'
     })
   end
 end
 
 When(/^I add a new product$/) do
   VCR.use_cassette('product_valid_brand') do
-    @product_response = ShipCompliant.client.call(:add_update_product, message: {
-      'Request' => {
-        'Security' => ShipCompliant.configuration.credentials,
-        'Product' => {
-          'BottleSizeMS' => 750,
-          'BrandKey' => 'DEN',
-          'DefaultCase' => 12,
-          'DefaultWholesaleCasePrice' => 270,
-          'Description' => 'Denver Snow Flake Cab 2013',
-          'PercentAlcohol' => 14.1,
-          'ProductDistribution' => 'Both',
-          'ProductKey' => 'SNFL13',
-          'ProductType' => 'Wine',
-          'UnitPrice' => 49.99,
-          'Varietal' => 'Cabernet Sauvignon',
-          'Vintage' => 2013,
-          'VolumeAmount' => 750,
-          'VolumeUnit' => 'milliliter'
-        },
-        'UpdateMode' => 'ErrorOnExisting'
-      }
+    @product_response = ShipCompliant.client.call(:add_update_product, {
+      'Product' => {
+        'BottleSizeMS' => 750,
+        'BrandKey' => 'DEN',
+        'DefaultCase' => 12,
+        'DefaultWholesaleCasePrice' => 270,
+        'Description' => 'Denver Snow Flake Cab 2013',
+        'PercentAlcohol' => 14.1,
+        'ProductDistribution' => 'Both',
+        'ProductKey' => 'SNFL13',
+        'ProductType' => 'Wine',
+        'UnitPrice' => 49.99,
+        'Varietal' => 'Cabernet Sauvignon',
+        'Vintage' => 2013,
+        'VolumeAmount' => 750,
+        'VolumeUnit' => 'milliliter'
+      },
+      'UpdateMode' => 'ErrorOnExisting'
     })
   end
 end
 
 When(/^I add a product that already exists$/) do
   VCR.use_cassette('product_already_exists') do
-    @product_response = ShipCompliant.client.call(:add_update_product, message: {
-      'Request' => {
-        'Security' => ShipCompliant.configuration.credentials,
-        'Product' => {
-          'BottleSizeMS' => 750,
-          'BrandKey' => 'DEN',
-          'DefaultCase' => 12,
-          'DefaultWholesaleCasePrice' => 270,
-          'Description' => 'Denver Snow Flake Cab 2013',
-          'PercentAlcohol' => 14.1,
-          'ProductDistribution' => 'Both',
-          'ProductKey' => 'SNFL13',
-          'ProductType' => 'Wine',
-          'UnitPrice' => 49.99,
-          'Varietal' => 'Cabernet Sauvignon',
-          'Vintage' => 2013,
-          'VolumeAmount' => 750,
-          'VolumeUnit' => 'milliliter'
-        },
-        'UpdateMode' => 'ErrorOnExisting'
-      }
+    @product_response = ShipCompliant.client.call(:add_update_product, {
+      'Product' => {
+        'BottleSizeMS' => 750,
+        'BrandKey' => 'DEN',
+        'DefaultCase' => 12,
+        'DefaultWholesaleCasePrice' => 270,
+        'Description' => 'Denver Snow Flake Cab 2013',
+        'PercentAlcohol' => 14.1,
+        'ProductDistribution' => 'Both',
+        'ProductKey' => 'SNFL13',
+        'ProductType' => 'Wine',
+        'UnitPrice' => 49.99,
+        'Varietal' => 'Cabernet Sauvignon',
+        'Vintage' => 2013,
+        'VolumeAmount' => 750,
+        'VolumeUnit' => 'milliliter'
+      },
+      'UpdateMode' => 'ErrorOnExisting'
     })
   end
 end
 
 When(/^I update a product$/) do
   VCR.use_cassette('update_product') do
-    @product_response = ShipCompliant.client.call(:add_update_product, message: {
-      'Request' => {
-        'Security' => ShipCompliant.configuration.credentials,
-        'Product' => {
-          'BottleSizeMS' => 750,
-          'BrandKey' => 'DEN',
-          'DefaultCase' => 12,
-          'DefaultWholesaleCasePrice' => 270,
-          'Description' => 'Denver Snow Flake Cab 2012',
-          'PercentAlcohol' => 14.1,
-          'ProductDistribution' => 'Both',
-          'ProductKey' => 'SNFL13',
-          'ProductType' => 'Wine',
-          'UnitPrice' => 49.99,
-          'Varietal' => 'Cabernet Sauvignon',
-          'Vintage' => 2012,
-          'VolumeAmount' => 750,
-          'VolumeUnit' => 'milliliter'
-        },
-        'UpdateMode' => 'UpdateExisting'
-      }
+    @product_response = ShipCompliant.client.call(:add_update_product, {
+      'Product' => {
+        'BottleSizeMS' => 750,
+        'BrandKey' => 'DEN',
+        'DefaultCase' => 12,
+        'DefaultWholesaleCasePrice' => 270,
+        'Description' => 'Denver Snow Flake Cab 2012',
+        'PercentAlcohol' => 14.1,
+        'ProductDistribution' => 'Both',
+        'ProductKey' => 'SNFL13',
+        'ProductType' => 'Wine',
+        'UnitPrice' => 49.99,
+        'Varietal' => 'Cabernet Sauvignon',
+        'Vintage' => 2012,
+        'VolumeAmount' => 750,
+        'VolumeUnit' => 'milliliter'
+      },
+      'UpdateMode' => 'UpdateExisting'
     })
   end
 end
 
 When(/^I ignore existing product on update$/) do
   VCR.use_cassette('ignore_existing_product') do
-    @product_response = ShipCompliant.client.call(:add_update_product, message: {
-      'Request' => {
-        'Security' => ShipCompliant.configuration.credentials,
-        'Product' => {
-          'BottleSizeMS' => 750,
-          'BrandKey' => 'DEN',
-          'DefaultCase' => 12,
-          'DefaultWholesaleCasePrice' => 270,
-          'Description' => 'Denver Snow Flake Cab 2012',
-          'PercentAlcohol' => 14.1,
-          'ProductDistribution' => 'Both',
-          'ProductKey' => 'SNFL13',
-          'ProductType' => 'Wine',
-          'UnitPrice' => 19.99,
-          'Varietal' => 'Cabernet Sauvignon',
-          'Vintage' => 2012,
-          'VolumeAmount' => 750,
-          'VolumeUnit' => 'milliliter'
-        },
-        'UpdateMode' => 'IgnoreExisting'
-      }
+    @product_response = ShipCompliant.client.call(:add_update_product, {
+      'Product' => {
+        'BottleSizeMS' => 750,
+        'BrandKey' => 'DEN',
+        'DefaultCase' => 12,
+        'DefaultWholesaleCasePrice' => 270,
+        'Description' => 'Denver Snow Flake Cab 2012',
+        'PercentAlcohol' => 14.1,
+        'ProductDistribution' => 'Both',
+        'ProductKey' => 'SNFL13',
+        'ProductType' => 'Wine',
+        'UnitPrice' => 19.99,
+        'Varietal' => 'Cabernet Sauvignon',
+        'Vintage' => 2012,
+        'VolumeAmount' => 750,
+        'VolumeUnit' => 'milliliter'
+      },
+      'UpdateMode' => 'IgnoreExisting'
     })
   end
 end
