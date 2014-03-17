41c44c3c9ebd74776d09c409fca7489ff0e02b61
Baylor Rae <baylorrae@gmail.com>
2014-03-16 22:37:27 -0500

    add docs for ShipmentCompliance

--- lib/ship_compliant/shipment_compliance.rb
+++ lib/ship_compliant/shipment_compliance.rb
@@ -1,14 +1,22 @@
 module ShipCompliant
+  # == ShipCompliant::ShipmentCompliance
+  #
+  # Wrap the +ShipmentComplianceResponse+ node.
   class ShipmentCompliance < Struct.new(:result)
 
+    # Checks if the shipment was compliant.
     def compliant?
       result[:is_compliant]
     end
 
+    # Returns the shipment key.
     def shipment_key
       result[:key]
     end
 
+    # Wraps the +RuleComplianceResponse+ nodes with ComplianceRule.
+    #
+    #   compliance_errors = shipment.rules.reject { |r| r.compliant? }
     def rules
       Array.wrap(result[:rules][:rule_compliance_response]).map do |rule|
         ComplianceRule.new(rule)
