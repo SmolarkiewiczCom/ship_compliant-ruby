6820127e672d442f9c1ef8c595cae50ee3d150d2
Baylor Rae <baylorrae@gmail.com>
2014-03-16 22:02:06 -0500

    add docs for SuggestedAddress

--- lib/ship_compliant/suggested_address.rb
+++ lib/ship_compliant/suggested_address.rb
@@ -1,38 +1,71 @@
 module ShipCompliant
+  # == ShipCompliant::SuggestedAddress
+  #
+  # This is a value object that wraps the +SuggestedAddress+ node.
+  #
+  #   address = ShipCompliant::SuggestedAddress.new(city: 'Boston', state: 'MA')
+  #   address.city #=> 'Boston'
+  #   address.state #=> 'MA'
   class SuggestedAddress < Struct.new(:address)
 
+    # Returns the suggested city.
     def city
       address[:city]
     end
     
+    # Returns the suggested county.
     def county
       address[:county]
     end
 
+    # Returns the suggested state.
     def state
       address[:state]
     end
 
+    # Returns the suggested street.
     def street1
       address[:street1]
     end
 
+    # Returns the suggested street line 2.
     def street2
       address[:street2]
     end
 
+    # Returns the suggested zip code.
     def zip1
       address[:zip1].to_i
     end
 
+    # Returns the suggested zip code extension.
     def zip2
       address[:zip2].to_i
     end
 
+    # Returns a Hash of the address location details.
+    #
+    # - +city_abbreviation+
+    # - +congressional_district+
+    # - +county_fips+
+    # - +time_zone+
+    # - +time_zone_code+
     def details
       address[:details]
     end
 
+    # Returns a Hash of the address parts.
+    #
+    # - +company+
+    # - +mail_box_name+
+    # - +mail_box_number+
+    # - +post_direction+
+    # - +pre_direction+
+    # - +street_name+
+    # - +street_number+
+    # - +street_suffix+
+    # - +suite_name+
+    # - +suite_number+
     def parts
       address[:parts]
     end
