99d6244c48b35ae316142504d53def6068f6ed80
Baylor Rae <baylorrae@gmail.com>
2014-03-23 11:02:26 -0500

    add steps for other cases

--- features/add_update_brand.feature
+++ features/add_update_brand.feature
@@ -8,9 +8,13 @@ Feature: Add Update Brand
     Then the brand should have been created
 
   Scenario: I add a brand that already exists
-    When pending
+    When I add a brand that already exists
+    Then I should get an error for already defined brand
 
   Scenario: I update a brand that already exists
-    When pending
+    When I update an existing brand
+    Then the brand should have been updated
 
   Scenario: I ignore an existing brand
+    When I ignore existing brand on update
+    Then the brand should have been updated
--- features/step_definitions/brand_steps.rb
+++ features/step_definitions/brand_steps.rb
@@ -1,23 +1,71 @@
-When(/^I add a new brand$/) do
-  VCR.use_cassette('add_brand') do
-    @brand_result = ShipCompliant::AddUpdateBrand.brand({
-      key: 'HCAS',
-      name: 'Hiccups Anonymous',
-      owner: {
-        city: 'Ye Old Brew Port',
-        country: 'US',
-        name: 'John Doe',
-        state: 'LA',
-        street1: '1337 Drunken Ave.',
-        zip: 84742
-      },
+When(/^I (create|update|ignore) a (?:new\s)?brand$/) do |action|
+  key = 'HCAS'
+  update_mode = 'ErrorOnExisting'
 
-      this_brand_is_bottled_by_a_third_party: false,
-      this_brand_is_produced_by_a_third_party: false,
-      this_brand_operates_under_a_trade_name: true,
-      this_brand_was_acquired_from_a_third_party: false
-    })
+  case action
+  when 'update'
+    key = 'HCAN'
+    update_mode = 'UpdateExisting'
+  when 'ignore'
+    update_mode = 'IgnoreExisting'
   end
+
+  @brand_result = ShipCompliant::AddUpdateBrand.brand({
+    key: key,
+    name: 'Hiccups Anonymous',
+    owner: {
+      city: 'Ye Old Brew Port',
+      country: 'US',
+      name: 'John Doe',
+      state: 'LA',
+      street1: '1337 Drunken Ave.',
+      zip: 84742
+    },
+
+    this_brand_is_bottled_by_a_third_party: false,
+    this_brand_is_produced_by_a_third_party: false,
+    this_brand_operates_under_a_trade_name: true,
+    this_brand_was_acquired_from_a_third_party: false
+  }, update_mode: update_mode)
+end
+
+When(/^I add a new brand$/) do
+  VCR.use_cassette('brand_valid') do
+    step "I create a brand"
+  end
+end
+
+When(/^I add a brand that already exists$/) do
+  VCR.use_cassette('brand_already_exists') do
+    step "I create a new brand"
+  end
+end
+
+When(/^I update an existing brand$/) do
+  VCR.use_cassette('brand_update_existing') do
+    step "I update a brand"
+  end
+end
+
+When(/^I ignore existing brand on update$/) do
+  VCR.use_cassette('brand_ignore_existing') do
+    step "I ignore a new brand"
+  end
+end
+
+Then(/^I should get an error for already defined brand$/) do
+  @brand_result.failure?.should be_true
+  @brand_result.errors.length.should == 1
+  error = @brand_result.errors.first
+  
+  error.code.should == 705
+  error.key.should == 'HCAS'
+  error.message.should == 'BrandKey  exists.'
+  error.type.should == 'Validation'
+end
+
+Then(/^the brand should have been updated$/) do
+  @brand_result.success?.should be_true
 end
 
 Then(/^the brand should have been created$/) do
